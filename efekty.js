6170b9b1cd754b6ae10e90829e83379d5392344f
ivosh128 <81686954+ivosh128@users.noreply.github.com>
2026-09-20 11:47:55 +0200

    Add files via upload

--- efekty.js
+++ efekty.js
@@ -16,6 +16,9 @@
     size();addEventListener('resize',size);
     var n=W<700?70:140;
     for(var i=0;i<n;i++){var z=Math.random();F.push({x:Math.random()*W,y:Math.random()*H,z:z,r:.6+z*z*3.2,s:.25+z*1.1,o:.15+z*.55,p:Math.random()*6.28})}
+    var E=[],m=W<700?10:20;
+    function spark(init){return{x:Math.random()*W,y:init?H*(.45+Math.random()*.55):H+5,s:.35+Math.random()*.6,r:1+Math.random()*1.5,o:.55+Math.random()*.4,p:Math.random()*6.28,g:Math.random(),l:init?Math.random()*200:0,m:260+Math.random()*320}}
+    for(var q=0;q<m;q++)E.push(spark(true));
     var wind=0,t=0;
     (function loop(){
       t+=.005;wind=Math.sin(t)*.6+.35;
@@ -25,6 +28,13 @@
         if(f.y>H+5){f.y=-5;f.x=Math.random()*W}
         if(f.x>W+5)f.x=-5;if(f.x<-5)f.x=W+5;
         x.beginPath();x.fillStyle='rgba(240,245,255,'+f.o+')';x.arc(f.x,f.y,f.r,0,6.283);x.fill();
+      }
+      // jiskry z ohně: pár teplých světýlek stoupá proti sněhu
+      for(var j=0;j<E.length;j++){var e=E[j];
+        e.y-=e.s;e.p+=.03;e.x+=Math.sin(e.p)*.5-wind*.15;e.l+=1;
+        var k=e.l/e.m;if(k>=1){E[j]=spark(false);continue}
+        var o=(k<.15?k/.15:1-(k-.15)/.85)*e.o*(.75+.25*Math.sin(e.p*4));
+        x.beginPath();x.fillStyle='rgba(255,'+(150+Math.round(e.g*50))+',80,'+o+')';x.shadowColor='rgba(255,120,50,.9)';x.shadowBlur=8;x.arc(e.x,e.y,e.r,0,6.283);x.fill();x.shadowBlur=0;
       }
       requestAnimationFrame(loop);
     })();
